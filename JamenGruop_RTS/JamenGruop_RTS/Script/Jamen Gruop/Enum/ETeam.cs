b272ea36b2ec15917d1b3c4909802f3b5de339e1
kasp050g <57252832+kasp050g@users.noreply.github.com>
2020-02-14 14:21:17 +0100

    Update

---
+++ JamenGruop_RTS/JamenGruop_RTS/Script/Jamen Gruop/Enum/ETeam.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JamenGruop_RTS.Script.Jamen_Gruop.Enum
+{
+	enum ETeam
+	{
+		Team01, Team02,
+		Team03, Team04,
+		Team05, Team06,
+		Team07, Team08
+	}
+}
---
+++ JamenGruop_RTS/JamenGruop_RTS/Script/Jamen Gruop/Units/Unit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JamenGruop_RTS.Script.Jamen_Gruop.Units
+{
+	class Unit
+	{
+	}
+}
