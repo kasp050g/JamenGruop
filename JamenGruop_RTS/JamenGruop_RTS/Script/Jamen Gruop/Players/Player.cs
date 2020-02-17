88143f6e2874da6dbbe728cc45281ba1cbdc537b
Nameuser0654 <57252652+Nameuser0654@users.noreply.github.com>
2020-02-17 12:26:27 +0100

    Barracks 1.0

---
+++ JamenGruop_RTS/JamenGruop_RTS/Script/Jamen Gruop/Players/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JamenGruop_RTS.Script.Jamen_Gruop.Players
+{
+	class Player
+	{
+		public int PlayerGold = 0;
+		public int PlayerWood = 0;
+		public int PlayerCurrentFood = 0;
+		public int PlayerMaxFood = 0;
+	}
+}
